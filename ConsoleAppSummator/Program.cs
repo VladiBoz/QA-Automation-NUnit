5df3404a40db5aeb1dc2f0030892e2db771a5929
VladiBoz <vladibozveliev@gmail.com>
2023-01-22 20:35:15 +0200

    New tests added

--- ConsoleAppSummator/Program.cs
+++ ConsoleAppSummator/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sumata e ravna na:" + Summator.susbtruction (new int[] {5,3,3}));
+            
         }
     }
 }
