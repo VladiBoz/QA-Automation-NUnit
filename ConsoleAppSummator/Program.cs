906b230120fab89f9a888103532c568f52637811
VladiBoz <vladibozveliev@gmail.com>
2023-01-22 16:35:01 +0200

    Code update

--- ConsoleAppSummator/Program.cs
+++ ConsoleAppSummator/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sumata e ravna na:" + Summator.Sum (new int[] {5,3,3}));
+            Console.WriteLine("Sumata e ravna na:" + Summator.susbtruction (new int[] {5,3,3}));
         }
     }
 }
--- ConsoleAppSummator/Summator.cs
+++ ConsoleAppSummator/Summator.cs
@@ -20,6 +20,59 @@ namespace ConsoleAppSummator
             return sum;
 
         }
+
+        public static int Average(int[] arr)
+        {
+            int sum = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                sum += arr[i];
+
+            }
+            return sum / arr.Length;
+                
+
+        }
+        public static int Multiply(int[] arr)
+        {
+            int sum = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                sum *= arr[i];
+
+            }
+            return sum;
+
+
+        }
+        public static int Devide(int[] arr)
+        {
+            int sum = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                sum /= arr[i];
+
+            }
+            return sum;
+
+
+        }
+        public static int susbtruction(int[] arr)
+        {
+            int sum = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                sum -= arr[i];
+
+            }
+            return sum;
+
+
+        }
     }
 }
 
