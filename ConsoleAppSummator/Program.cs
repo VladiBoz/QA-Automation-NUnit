67c4838e43b24d343c5846c1da4b8f84779b6f00
CustomCoding <CustomCoding@DESKTOP-UH65DAS>
2023-01-22 10:42:20 +0200

    Add project files.

---
+++ ConsoleAppSummator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppSummator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sumata e ravna na:" + Summator.Sum (new int[] {5,3,3}));
+        }
+    }
+}
