32133a352b727dbb3bbe480c77670ee04a5d7db6
Vladislav Bozveliev <vladibozveliev@gmail.com>
2023-02-02 17:31:52 +0200

    New test and 100 % test covarage

--- NUnit_CollectionTests/UnitTest1.cs
+++ NUnit_CollectionTests/UnitTest1.cs
@@ -1,6 +1,5 @@
 using Collections;
-using NUnit.Framework.Constraints;
-using System.Globalization;
+
 
 namespace Collection.UnitTests
 {
@@ -10,137 +9,164 @@ namespace Collection.UnitTests
         [Test]
         public void Test_Collection_EmptyConstructor()
         {
-            //Arange
-            var coll = new Collection<int>();
+            //Arrange
+            string expectedCollection = "[]";
+
+            //Act
+            var collection = new Collection<int>();
 
             //Assert
-            Assert.AreEqual(coll.ToString(), "[]");
+            Assert.That(expectedCollection, Is.EqualTo(collection.ToString()));
         }
-        [Test]
-        public void Test_Collection_ConstructorSigleItem()
-        {
-            //Arange
-            var coll = new Collection<int>(5);
 
-            //Assert
-            Assert.AreEqual(coll.ToString(), "[5]");
-        }
-        [Test]
-        public void Test_Collection_ConstructorMultipleItems()
-        {
-            //Arange
-            var coll = new Collection<int>(5, 2);
 
-            //Assert
-            Assert.AreEqual(coll.ToString(), "[5, 2]");
-        }
         [Test]
         public void Test_Collection_Add()
         {
-            //Arange
-            var coll = new Collection<int>();
+            //Arrange
+            var collection = new Collection<int>();
 
             //Act
-            coll.Add(5);
-            coll.Add(6);
+            collection.Add(5);
 
             //Assert
-            Assert.AreEqual(coll.ToString(), "[5, 6]");
+            Assert.AreEqual(collection.ToString(), "[5]");
         }
         [Test]
         public void Test_Collection_CountAndCapacity()
         {
-            //Arange
-            var coll = new Collection<int>(2, 9);
+            //Arrange
+            var collection = new Collection<int>(2, 9);
 
             //Assert
-            Assert.AreEqual(coll.Count, 2);
-            Assert.That(coll.Capacity, Is.GreaterThan(coll.Count));
+            Assert.AreEqual(collection.Count, 2);
+            Assert.That(collection.Capacity, Is.GreaterThan(collection.Count));
         }
         [Test]
         public void Test_Collection_GetByIndex()
         {
-            //Arange
-            var names = new Collection<string>("Vladi", "Poly");
+            //Arrange
+            var names = new Collection<string>("Vladi");
 
             //Assert
             Assert.AreEqual(names[0], "Vladi");
-            Assert.AreEqual(names[1], "Poly");
+
         }
         [Test]
         public void Test_Collection_SetByIndex()
         {
-            //Arange
-            var names = new Collection<string>("Vladi", "Poly");
+            //Arrange
+            var names = new Collection<string>("Vladi");
 
             //Act
             names[0] = "Gosho";
 
             //Assert
             Assert.AreEqual(names[0], "Gosho");
-            Assert.AreEqual(names[1], "Poly");
+
         }
         [Test]
         public void Test_Collection_Clear()
         {
-            //Arange
-            var coll = new Collection<int>(5, 6);
+            //Arrange
+            var collection = new Collection<int>(5, 6);
             //Act
-            coll.Clear();
+            collection.Clear();
             //Assert
-            Assert.AreEqual(coll.ToString(), "[]");
+            Assert.AreEqual(collection.ToString(), "[]");
         }
         [Test]
         public void Test_Collection_AddRange()
         {
-            //Arange
-            var coll = new Collection<string>("Vladi", "Desi", "Poly");
+            //Arrange
+            var collection = new Collection<string>("Vladi", "Desi", "Poly");
 
             //Act
-            coll.AddRange("Peter");
+            collection.AddRange("Peter");
 
             //Assert
-            Assert.AreEqual(coll.ToString(), "[Vladi, Desi, Poly, Peter]");
+            Assert.AreEqual(collection.ToString(), "[Vladi, Desi, Poly, Peter]");
+
         }
         [Test]
         public void Test_Collection_InsertAt()
         {
-            //Arange
-            var coll = new Collection<string>("Vladi");
+            //Arrange
+            var collection = new Collection<string>("Vladi");
 
             //Act
-            coll.InsertAt(1, "1");
+            collection.InsertAt(1, "1");
+
 
             //Assert
-            Assert.AreEqual(coll.ToString(), "[Vladi, 1]");
+            Assert.AreEqual(collection.ToString(), "[Vladi, 1]");
 
         }
         [Test]
         public void Test_Collection_RemoveAt()
         {
-            //Arange
-            var coll = new Collection<string>("Vladi", "Boz");
+            //Arrange
+            var collection = new Collection<string>("Vladi", "Boz");
 
             //Act
-            coll.RemoveAt(0);
+            collection.RemoveAt(0);
 
             //Assert
-            Assert.AreEqual(coll.ToString(), "[Boz]");
+            Assert.AreEqual(collection.ToString(), "[Boz]");
 
+        }
+
+        [Test]
+        public void Test_Collection_Exchange()
+        {   //Arrange
+            var collection = new Collection<string>("Bozveliev", "Vladislav");
+
+            //Act
+            collection.Exchange(0, 1);
+
+            //Assert
+            Assert.AreEqual(collection.ToString(), "[Vladislav, Bozveliev]");
+        }
+
+        [Test]
+        public void Test_Collection_GrowCapacity()
+        {    //Arrange
+            var collection = new Collection<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+            //Act
+            for (int i = 1; i <= 20; i++)
+                collection.Add(i);
+            //Assert
+            Assert.AreEqual(collection.ToString(), "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]");
         }
         [Test]
-        public void Test_Collection_CheckRange()
+        public void Test_Collection_ByInvalidIndex()
         {
-            //Arange
-            var coll = new Collection<string>("Vladi", "Boz");
-
-            //Act
-            coll.RemoveAt(0);
+            //Arrange
+            var collection = new Collection<string>("Vladi", "Peter");
 
             //Assert
-            Assert.AreEqual(coll.ToString(), "[Boz]");
-
+            Assert.That(() => { var name = collection[-1]; },
+        Throws.InstanceOf<ArgumentOutOfRangeException>());
+            Assert.That(() => { var name = collection[2]; },
+      Throws.InstanceOf<ArgumentOutOfRangeException>());
+            Assert.That(() => { var name = collection[500]; },
+     Throws.InstanceOf<ArgumentOutOfRangeException>());
+            Assert.AreEqual(collection.ToString(), "[Vladi, Peter]");
         }
 
+
+        [Test]
+        public void Test_Collection_InsertAtWithGrow()
+        {
+            var collection = new Collection<int>(1, 2, 3);
+            int oldCapacity = collection.Capacity;
+            var newCollection = Enumerable.Range(10, 50).ToArray();
+            collection.AddRange(newCollection);
+            collection.InsertAt(20, 50);
+            string expectedCollection = "[" + string.Join(", ", newCollection) + "]";
+            Assert.AreNotEqual(collection.ToString(), expectedCollection);
+            Assert.That(collection.Capacity, Is.GreaterThanOrEqualTo(oldCapacity));
+            Assert.That(collection.Capacity, Is.GreaterThanOrEqualTo(collection.Count));
+        }
     }
 }
